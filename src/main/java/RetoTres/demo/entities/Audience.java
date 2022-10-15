17e49641e74ab6c6b4cd75343605de42e6eb0d9c
jsebasg <juan.garzon.mt@usa.edu.co>
2022-10-15 15:40:39 -0500

    prueba Cambio

--- src/main/java/RetoTres/demo/entities/Audience.java
+++ src/main/java/RetoTres/demo/entities/Audience.java
@@ -45,7 +45,7 @@ public class Audience implements Serializable {
     private Category category;
     
     @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "audience")
-    @JsonIgnoreProperties({"audience", "client"})
+    @JsonIgnoreProperties({"audience"})
     private List <Message> messages;
     
     @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "audience")
